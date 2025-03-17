2266b05fe8eba9d4d2db3d7b408eeed2fad7a275
bfyqjzsgkqycf <19128317516@163.com>
2025-03-17 18:15:24 +0800

    feat: 添加子弹类并实现移动和越界处理
    Bullet类已从简单的定义扩展为包含移动速度、初始化方法、移动方法和越界处理方法的完整类。现在子弹类继承自FlyingObject类，并实现了step和outOfBounds方法。

--- src/Bullet.java
+++ src/Bullet.java
@@ -1,2 +1,34 @@
-public class Bullet {
+package com.tarena.fly;
+
+/**
+ * 子弹类:是飞行物
+ */
+public class Bullet extends FlyingObject {
+    private int speed = 3;  //移动的速度
+
+    /**
+     * 初始化数据
+     */
+    public Bullet(int x, int y) {
+        this.x = x;
+        this.y = y;
+        this.image = ShootGame.bullet;
+    }
+
+    /**
+     * 移动
+     */
+    @Override
+    public void step() {
+        y -= speed;
+    }
+
+    /**
+     * 越界处理
+     */
+    @Override
+    public boolean outOfBounds() {
+        return y < -height;
+    }
+
 }
